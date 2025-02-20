580bc5bbf1cfc9c60778a4fea4086c22ce97c3bd
Kevin Herron <kevinherron@gmail.com>
2025-02-20 05:13:31 -0800

    Fix minor inspections, move verbose static fields

--- milo-examples/server-examples/src/main/java/org/eclipse/milo/examples/server/ExampleNamespace.java
+++ milo-examples/server-examples/src/main/java/org/eclipse/milo/examples/server/ExampleNamespace.java
@@ -12,7 +12,6 @@ package org.eclipse.milo.examples.server;
 
 import static org.eclipse.milo.opcua.stack.core.types.builtin.unsigned.Unsigned.ubyte;
 import static org.eclipse.milo.opcua.stack.core.types.builtin.unsigned.Unsigned.uint;
-import static org.eclipse.milo.opcua.stack.core.types.builtin.unsigned.Unsigned.ulong;
 import static org.eclipse.milo.opcua.stack.core.types.builtin.unsigned.Unsigned.ushort;
 
 import java.lang.reflect.Array;
@@ -77,71 +76,6 @@ import org.slf4j.LoggerFactory;
 public class ExampleNamespace extends ManagedNamespaceWithLifecycle {
 
   public static final String NAMESPACE_URI = "urn:eclipse:milo:hello-world";
-
-  private static final Object[][] STATIC_SCALAR_NODES =
-      new Object[][] {
-        {"Boolean", NodeIds.Boolean, Variant.ofBoolean(false)},
-        {"Byte", NodeIds.Byte, Variant.ofByte(ubyte(0x00))},
-        {"SByte", NodeIds.SByte, Variant.ofSByte((byte) 0x00)},
-        {"Integer", NodeIds.Integer, Variant.ofInt32(32)},
-        {"Int16", NodeIds.Int16, Variant.ofInt16((short) 16)},
-        {"Int32", NodeIds.Int32, Variant.ofInt32(32)},
-        {"Int64", NodeIds.Int64, Variant.ofInt64(64L)},
-        {"UInteger", NodeIds.UInteger, Variant.ofUInt32(uint(32))},
-        {"UInt16", NodeIds.UInt16, Variant.ofUInt16(ushort(16))},
-        {"UInt32", NodeIds.UInt32, Variant.ofUInt32(uint(32))},
-        {"UInt64", NodeIds.UInt64, Variant.ofUInt64(ulong(64L))},
-        {"Float", NodeIds.Float, Variant.ofFloat(3.14f)},
-        {"Double", NodeIds.Double, Variant.ofDouble(3.14d)},
-        {"String", NodeIds.String, Variant.ofString("string value")},
-        {"DateTime", NodeIds.DateTime, Variant.ofDateTime(DateTime.now())},
-        {"Guid", NodeIds.Guid, Variant.ofGuid(UUID.randomUUID())},
-        {
-          "ByteString",
-          NodeIds.ByteString,
-          Variant.ofByteString(new ByteString(new byte[] {0x01, 0x02, 0x03, 0x04}))
-        },
-        {"XmlElement", NodeIds.XmlElement, Variant.ofXmlElement(new XmlElement("<a>hello</a>"))},
-        {
-          "LocalizedText",
-          NodeIds.LocalizedText,
-          Variant.ofLocalizedText(LocalizedText.english("localized text"))
-        },
-        {
-          "QualifiedName",
-          NodeIds.QualifiedName,
-          Variant.ofQualifiedName(new QualifiedName(1234, "defg"))
-        },
-        {"NodeId", NodeIds.NodeId, Variant.ofNodeId(new NodeId(1234, "abcd"))},
-        {"Variant", NodeIds.BaseDataType, Variant.ofInt32(32)},
-        {"Duration", NodeIds.Duration, Variant.ofDouble(1.0)},
-        {"UtcTime", NodeIds.UtcTime, Variant.ofDateTime(DateTime.now())},
-      };
-
-  private static final Object[][] STATIC_ARRAY_NODES =
-      new Object[][] {
-        {"BooleanArray", NodeIds.Boolean, false},
-        {"ByteArray", NodeIds.Byte, ubyte(0)},
-        {"SByteArray", NodeIds.SByte, (byte) 0x00},
-        {"Int16Array", NodeIds.Int16, (short) 16},
-        {"Int32Array", NodeIds.Int32, 32},
-        {"Int64Array", NodeIds.Int64, 64L},
-        {"UInt16Array", NodeIds.UInt16, ushort(16)},
-        {"UInt32Array", NodeIds.UInt32, uint(32)},
-        {"UInt64Array", NodeIds.UInt64, ulong(64L)},
-        {"FloatArray", NodeIds.Float, 3.14f},
-        {"DoubleArray", NodeIds.Double, 3.14d},
-        {"StringArray", NodeIds.String, "string value"},
-        {"DateTimeArray", NodeIds.DateTime, DateTime.now()},
-        {"GuidArray", NodeIds.Guid, UUID.randomUUID()},
-        {
-          "ByteStringArray", NodeIds.ByteString, new ByteString(new byte[] {0x01, 0x02, 0x03, 0x04})
-        },
-        {"XmlElementArray", NodeIds.XmlElement, new XmlElement("<a>hello</a>")},
-        {"LocalizedTextArray", NodeIds.LocalizedText, LocalizedText.english("localized text")},
-        {"QualifiedNameArray", NodeIds.QualifiedName, new QualifiedName(1234, "defg")},
-        {"NodeIdArray", NodeIds.NodeId, new NodeId(1234, "abcd")}
-      };
 
   private final Logger logger = LoggerFactory.getLogger(getClass());
 
@@ -323,7 +257,7 @@ public class ExampleNamespace extends ManagedNamespaceWithLifecycle {
     getNodeManager().addNode(arrayTypesFolder);
     rootNode.addOrganizes(arrayTypesFolder);
 
-    for (Object[] os : STATIC_ARRAY_NODES) {
+    for (Object[] os : ExampleData.STATIC_ARRAY_NODES) {
       String name = (String) os[0];
       NodeId typeId = (NodeId) os[1];
       Object value = os[2];
@@ -372,7 +306,7 @@ public class ExampleNamespace extends ManagedNamespaceWithLifecycle {
     getNodeManager().addNode(scalarTypesFolder);
     rootNode.addOrganizes(scalarTypesFolder);
 
-    for (Object[] os : STATIC_SCALAR_NODES) {
+    for (Object[] os : ExampleData.STATIC_SCALAR_NODES) {
       String name = (String) os[0];
       NodeId typeId = (NodeId) os[1];
       Variant variant = (Variant) os[2];
@@ -454,11 +388,8 @@ public class ExampleNamespace extends ManagedNamespaceWithLifecycle {
         .addLast(
             new RestrictedAccessFilter(
                 identity -> {
-                  if (identity instanceof Identity.UsernameIdentity) {
-                    Identity.UsernameIdentity usernameIdentity =
-                        (Identity.UsernameIdentity) identity;
-
-                    if (usernameIdentity.getUsername().equals("admin")) {
+                  if (identity instanceof Identity.UsernameIdentity ui) {
+                    if (ui.getUsername().equals("admin")) {
                       return AccessLevel.READ_WRITE;
                     } else {
                       return AccessLevel.NONE;
@@ -500,11 +431,8 @@ public class ExampleNamespace extends ManagedNamespaceWithLifecycle {
         .addLast(
             new RestrictedAccessFilter(
                 identity -> {
-                  if (identity instanceof Identity.UsernameIdentity) {
-                    Identity.UsernameIdentity usernameIdentity =
-                        (Identity.UsernameIdentity) identity;
-
-                    if (usernameIdentity.getUsername().equals("admin")) {
+                  if (identity instanceof Identity.UsernameIdentity ui) {
+                    if (ui.getUsername().equals("admin")) {
                       return AccessLevel.READ_WRITE;
                     } else {
                       return AccessLevel.NONE;
